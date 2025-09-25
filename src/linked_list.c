03cec2d432b63ac2449277d46baf00a3f1ef7a65
alifoo <alissonayres.trb@gmail.com>
2025-09-25 07:37:17 -0300

    feat: ll insert after and find node

--- src/linked_list.c
+++ src/linked_list.c
@@ -30,6 +30,21 @@ node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
     return node_to_insert;
 }
 
+node_t *find_node(node_t *head, int value) {
+    node_t *tmp = head;
+    while (tmp != NULL) {
+        if (tmp->value == value)
+            return tmp;
+        tmp = tmp->next;
+    }
+    return NULL;
+}
+
+void insert_after_node(node_t *node_to_insert_after, node_t *newnode) {
+    newnode->next = node_to_insert_after->next;
+    node_to_insert_after->next = newnode;
+}
+
 int main() {
     node_t *head = NULL;
     node_t *tmp;
@@ -38,6 +53,13 @@ int main() {
         tmp = create_new_node(i);
         insert_at_head(&head, tmp);
     }
+
+    tmp = find_node(head, 13);
+    printf("found node with value %d\n", tmp->value);
+
+    printlist(head);
+    insert_after_node(tmp, create_new_node(75));
+
     printlist(head);
 
     return 0;
